af2c73905b592517fd50010d81d79d8edb102ebd
finn995 <finn995@tutanota.de>
2016-12-16 10:54:10 +0200

    completed 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add(num1, num2)
+	return num1 + num2
+end
+
+def subtract(num1, num2)
+	return num1 - num2
+end
+
+def sum(nums)
+	sumnum = 0
+	nums.each do |num|
+		sumnum = sumnum + num
+	end
+	return sumnum
+end
